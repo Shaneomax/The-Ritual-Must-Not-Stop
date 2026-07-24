58b35ad9e0c23b8d957789a9e16f45d6c7eca377
Anik Pal <100011710+Shaneomax@users.noreply.github.com>
2026-07-24 12:06:55 +0600

    Door Animation added

--- Assets/V0/Scripts/Interactables/Door/InteractableDoor.cs
+++ Assets/V0/Scripts/Interactables/Door/InteractableDoor.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class InteractableDoor : MonoBehaviour, IInteractable
 {
+    [Header("Door Settings")]
     public bool isOpen = false;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     public string GetDescription()
     {
@@ -11,8 +18,11 @@ public class InteractableDoor : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-        isOpen = !isOpen;
+        isOpen = !isOpen; 
         Debug.Log(isOpen ? "Door Opened!" : "Door Closed!");
-        // Add your Animator or Transform rotation logic here
+        if (_animator != null)
+        {
+            _animator.SetBool("IsOpen", isOpen);
+        }
     }
 }
